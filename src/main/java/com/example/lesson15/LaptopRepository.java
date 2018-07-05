0853d636cee6bd7c706699595361691cd294c268
josmyJJ <josemyjoseph@gmail.com>
2018-07-05 16:03:43 -0400

    initial commit

---
+++ src/main/java/com/example/lesson15/LaptopRepository.java
@@ -0,0 +1,6 @@
+package com.example.lesson15;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface LaptopRepository extends CrudRepository<Laptop, Long> {
+}
---
+++ src/main/java/com/example/lesson15/Lesson15Application.java
@@ -0,0 +1,12 @@
+package com.example.lesson15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson15Application.class, args);
+	}
+}
